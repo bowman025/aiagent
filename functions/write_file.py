5ecd6bcaf8c4ef3174912116b396bde2f4d30a56
bowman025 <rastko.andonov@gmail.com>
2025-12-29 18:33:17 +0100

    Write and add run_python_file and test

--- functions/write_file.py
+++ functions/write_file.py
@@ -5,7 +5,7 @@ def write_file(working_directory, file_path, content):
         working_dir_abs = os.path.abspath(working_directory)
         target_file = os.path.normpath(os.path.join(working_dir_abs, file_path))
         is_within_working_dir  = os.path.commonpath([working_dir_abs, target_file]) == working_dir_abs
-        if not is_within_working_dir :
+        if not is_within_working_dir:
             return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
         if os.path.isdir(target_file):
             return f'Error: Cannot write to "{file_path}" as it is a directory'
